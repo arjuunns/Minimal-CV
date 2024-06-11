a071c12b4dcafb4b8dbd77852025c1e79436db58
codermal <144340917+arjuunns@users.noreply.github.com>
2024-06-12 00:14:51 +0530

    Update resume.js

--- resume.js
+++ resume.js
@@ -1,26 +1,31 @@
 const button = document.querySelector('.button');
 const body = document.querySelector('body');
-
-let isDarkMode = false;
+const links = document.querySelectorAll('a');
+let isDarkMode = true;
 
 button.addEventListener('click', function(e) {
     if (isDarkMode) {
         // Switch to light mode
         body.style.backgroundColor = 'white';
         body.style.color = 'black';
-        body.style.borderColor = 'black';
         button.innerHTML = "Switch to Dark Mode";
-        button.style.backgroundColor = 'black'
-        button.style.color='white'
+        button.style.backgroundColor = 'black';
+        button.style.color = 'white';
         
-        } else {
-            // Switch to dark mode
-            body.style.backgroundColor = '#212121';
-            body.style.color = 'white';
-            body.style.borderColor = 'white';
-            button.innerHTML = "Switch to Light Mode";
-            button.style.backgroundColor = 'white'
-            button.style.color='black'
+        links.forEach(link => {
+            link.style.color = 'rgb(23, 88, 192);'; // Change to light mode link color
+        });
+    } else {
+        // Switch to dark mode
+        body.style.backgroundColor = '#212121';
+        body.style.color = 'white';
+        button.innerHTML = "Switch to Light Mode";
+        button.style.backgroundColor = 'white';
+        button.style.color = 'black';
+        
+        links.forEach(link => {
+            link.style.color = 'rgb(72, 146, 220)'; // Change to dark mode link color
+        });
     }
     isDarkMode = !isDarkMode; // Toggle the mode
 });
