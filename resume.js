00fb076d21ba81aa9e9477f79b7f6a28ed5d133a
codermal <144340917+arjuunns@users.noreply.github.com>
2024-06-13 07:55:34 +0530

    Update resume.js

--- resume.js
+++ resume.js
@@ -6,7 +6,7 @@ let isDarkMode = true;
 button.addEventListener("click", function (e) {
   if (isDarkMode) {
     body.style.background =
-      "radial-gradient(circle at 10% 20%, rgb(239, 246, 249) 0%, rgb(206, 239, 253) 90%)"; // dark mode
+      "radial-gradient(circle, rgb(239, 246, 249) 0%, rgb(206, 239, 253) 90%)"; // dark mode
     body.style.color = "black";
     body.style.backgroundSize = "cover";
     links.forEach((link) => {
@@ -15,7 +15,7 @@ button.addEventListener("click", function (e) {
   } else {
     body.style.color = "white";
     body.style.background =
-      "radial-gradient(circle at 24.1% 68.8%, rgb(50, 50, 50) 0%, rgb(0, 0, 0) 99.4%)"; // Dark mode
+      "radial-gradient(circle, rgb(50, 50, 50) 0%, rgb(0, 0, 0) 99.4%)"; // Dark mode
     body.style.backgroundSize = "cover";
     links.forEach((link) => {
       link.style.color = "rgb(72, 146, 220)";
